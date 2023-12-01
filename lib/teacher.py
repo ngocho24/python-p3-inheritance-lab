bc9ba9f3587c4eb1f853775a3d087ed3267e5bac
Elijah Ngocho <elijahngocho24@gmail.com>
2023-12-01 13:29:28 +0300

    completed

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
-from user import User
+from lib.user import User
 
 import random
 
-class Teacher(User):
+class User:
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
 
-    def teach(self):
-        pass
+class Teacher(User):
+    def __init__(self, first_name, last_name, knowledge=None):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge if knowledge is not None else []
